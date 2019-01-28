383285f0ca67882ca978c48e1c858174f57c9611
deniskonovalov <denis.konov93@gmail.com>
2019-01-28 15:40:37 +0200

    Add GET /users/me

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middlewares/authenticate');
 
 const port = process.env.PORT || 3000;
 
@@ -107,6 +108,10 @@ app.post('/users', (req, res) => {
     }).catch((error) => {
         res.status(400).send(error);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
