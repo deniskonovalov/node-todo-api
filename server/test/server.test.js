dd95cb0ba0b54e30ca53a7a96c8497e261130286
deniskonovalov <denis.konov93@gmail.com>
2019-01-31 23:01:33 +0200

    Add tes for POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -255,3 +255,52 @@ describe('POST /users', () => {
             .end(done)
     });
 });
+
+describe('POST /users/login', () => {
+    it('Should login user and return token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: 'testuser2@mail.com',
+                password: 'password'
+            })
+            .expect(200)
+            .expect(res => {
+                expect(res.headers['x-auth']).toExist();
+            }).end((err,res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then(user => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('Should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: 'testuser2@mail.com',
+                password: 'password123'
+            })
+            .expect(400)
+            .expect(res => {
+                expect(res.headers['x-auth']).toNotExist();
+            }).end((err,res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
